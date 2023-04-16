6930b7a9bba683d6a710c7f4c2ad236c41741c2d
Joíle Júnior <joilejunior2@gmail.com>
2023-04-16 18:07:15 -0300

    80% da tela de login ok

--- lib/view/login.view.dart
+++ lib/view/login.view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:loja_de_roupas/utils/global.colors.dart';
+import 'package:loja_de_roupas/view/widgets/button.global.dart';
+import 'package:loja_de_roupas/view/widgets/text.form.global.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+  LoginView({Key? key}) : super(key: key);
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,29 +14,48 @@ class LoginView extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
-            color: Colors.green,
             width: double.infinity,
             padding: const EdgeInsets.all(15.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Loja de roupas', 
-                  style: TextStyle(
-                    color: GlobalColors.mainColor,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                Text(
-                    'Login to your accoun', 
+                const SizedBox(height: 20),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Loja de roupas',
                     style: TextStyle(
-                      color: GlobalColors.textColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+                      color: GlobalColors.mainColor,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                 
+                const SizedBox(height: 50),
+                Text(
+                  'Faça o login em sua conta',
+                  style: TextStyle(
+                    color: GlobalColors.textColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                /// Email input
+                TextFormGlobal(
+                  controller: emailController, 
+                  text: 'Email', obscure: false, 
+                  textInputType: TextInputType.emailAddress
+                ),
+                const SizedBox(height: 10),
+                /// Senha input
+                TextFormGlobal(
+                  controller: passwordController, 
+                  text: 'Senha', obscure: true, 
+                  textInputType: TextInputType.text
+                ),
+                const SizedBox(height: 10),
+                ButtonGlobal(),
               ],
             ),
           ),
--- lib/view/splash.view.dart
+++ lib/view/splash.view.dart
@@ -11,7 +11,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Timer(const Duration(seconds: 1), () {
-      Get.to(const LoginView());
+      Get.to(LoginView());
     });
 
     return Scaffold(
